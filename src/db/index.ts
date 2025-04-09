7ea2a2098ec970da270e1e49a9c7b40df5a5361a
0-don <70039285+0-don@users.noreply.github.com>
2025-04-10 01:59:16 +0200

    easy

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,10 @@
+import { log } from "console";
 import { drizzle } from "drizzle-orm/bun-sql";
+import { migrate } from "drizzle-orm/bun-sql/migrator";
+import { resolve } from "path";
 
 export const db = drizzle(process.env.DATABASE_URL!);
+
+migrate(db, { migrationsFolder: resolve("drizzle") })
+  .then(() => log("Database migrated successfully"))
+  .catch(() => process.exit(1));
