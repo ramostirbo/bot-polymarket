e3902c95ae986f5b6dbf754b1d965da09be2e28d
0-don <don.cryptus@gmail.com>
2025-07-09 21:57:15 +0200

    fix: add garbage collection and sleep functionality to market synchronization loop

--- src/markets.ts
+++ src/markets.ts
@@ -1,10 +1,17 @@
 import "@dotenvx/dotenvx/config";
+import { sleep } from "bun";
 import { error } from "console";
 import { syncMarkets } from "./polymarket/markets";
 
 async function main() {
   while (true) {
     await syncMarkets();
+    
+    if (typeof global.gc === "function") {
+      global.gc();
+    }
+
+    await sleep(60000);
   }
 }
 
